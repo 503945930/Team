603c396524faef540bcd20752c8a0a9ea2c5d766
weiy <503945930@qq.com>
2016-05-16 14:54:11 +0800

    update header

--- code/src/components/Header.jsx
+++ code/src/components/Header.jsx
@@ -12,8 +12,6 @@ class Header extends Component {
     }
   }
 
-
-
   // 打开语言下拉选择
   handleShowLanghOnclick(e) {
     this.setState({
@@ -39,62 +37,63 @@ class Header extends Component {
   render() {
     return (
       <header className="site-header navbar navbar-static-top bs-docs-nav " id="top" role="banner">
-        <div className="container">
-          <div className="navbar-header">
-            <button className="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a href="#" className="navbar-brand">Teambition</a>
+        <div className="navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button className="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a href="#" className="navbar-brand">Teambition</a>
+            </div>
+            <nav id="bs-navbar" className="collapse navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li className="active">
+                  <Link to="/">
+                    <FormattedMessage id="home"/>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/About">
+                    <FormattedMessage id="about"/>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/Contactus">
+                    <FormattedMessage id="contactus"/>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/">
+                    <FormattedMessage id="blog"/>
+                  </Link>
+                </li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li className="dropdown">
+                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" onClick={this.handleShowLanghOnclick.bind(this)} aria-haspopup="true" aria-expanded="false">
+                    语言<span className="caret"></span>
+                  </a>
+                  <ul className={this.state.showLang
+                    ? 'dropdown-menu hidden'
+                    : 'dropdown-menu visible'}>
+                    <li>
+                      <a href="#" onClick={this.handleLanghOnclick.bind(this, "zh")}>中文版</a>
+                    </li>
+                    <li>
+                      <a href="#" onClick={this.handleLanghOnclick.bind(this, "en")}>English</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
           </div>
-          <nav id="bs-navbar" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li className="active">
-                <Link to="/">
-                  <FormattedMessage id="home"/>
-                </Link>
-              </li>
-              <li>
-                <Link to="/About">
-                  <FormattedMessage id="about"/>
-                </Link>
-              </li>
-              <li>
-                <Link to="/Contactus">
-                  <FormattedMessage id="contactus"/>
-                </Link>
-              </li>
-              <li>
-                <Link to="/">
-                  <FormattedMessage id="blog"/>
-                </Link>
-              </li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" onClick={this.handleShowLanghOnclick.bind(this)} aria-haspopup="true" aria-expanded="false">
-                  语言<span className="caret"></span>
-                </a>
-                <ul className={this.state.showLang
-                  ? 'dropdown-menu hidden'
-                  : 'dropdown-menu visible'}>
-                  <li>
-                    <a href="#" onClick={this.handleLanghOnclick.bind(this, "zh")}>中文版</a>
-                  </li>
-                  <li>
-                    <a href="#" onClick={this.handleLanghOnclick.bind(this, "en")}>English</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
         </div>
-
         <div className="jumbotron">
-          <Gallery />
-        
+          <Gallery/>
+
         </div>
       </header>
 
